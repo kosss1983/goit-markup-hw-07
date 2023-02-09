2154e0c54362b83aa4e26d8ed027cdb19d4aff8f
constantine1983 <constantineck83@gmail.com>
2023-02-10 01:11:18 +0200

    complete module 7

--- js/modal.js
+++ js/modal.js
@@ -9,6 +9,7 @@
     refs.closeModalBtn.addEventListener("click", toggleModal);
 
     function toggleModal() {
+        console.log("here");
         refs.modal.classList.toggle("is-hidden");
     }
 })();
